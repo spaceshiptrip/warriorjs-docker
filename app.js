e2224a863c6c9fb457278fbbc81830399d992fee
spaceshiptrip <4379326+spaceshiptrip@users.noreply.github.com>
2021-01-19 11:02:14 -0800

    first commit specific to spaceshiptrip

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!! From your Simple Express Web-Server!')
-})
+// just server the local directory
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var port = 8080;
+var __dirname = 'warriorjs'
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+connect().use(serveStatic(__dirname)).listen(port, function(){
+        console.log('Server running on ' + port + '...');
+});
 
+ 
